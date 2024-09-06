0de1d64f465c207327c4d7f419d3b40cc38d2385
Pedro Teixeira <pedro15at@gmail.com>
2024-09-06 15:08:19 -0300

    feat: making the responsivity and links

--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,10 @@
 import { createGlobalStyle } from 'styled-components'
 
+export const breakpoints = {
+  desktop: '1024px',
+  tablet: '768px'
+}
+
 export const cores = {
   branca: '#EEE',
   preta: '#111',
@@ -27,5 +32,9 @@ body {
   max-width: 1024px;
   width: 100%;
   margin: 0 auto;
+
+  @media (max-width: ${breakpoints.desktop}){
+    max-width: 80%;
+  }
 }
 `
